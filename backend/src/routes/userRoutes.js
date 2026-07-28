f2c65113e60cf0d65bca0e975c3f80455400eb11
Prashant-Pal23 <prashantpal23225@gmail.com>
2026-07-29 02:56:12 +0530

    Adding multer concept

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,33 @@
 import express from "express";
+import multer from "multer"
 import protect from "../middlewares/authMiddleware.js";
-import { changeUserPassword, getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { changeUserPassword, getUserProfile, updateUserProfile, uploadProfilePicture } from "../controllers/userController.js";
 
 const router = express.Router();
+
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+
+        if (!file.mimetype.startsWith("image/")) {
+            return cb(new Error("Only image files are allowed"));
+        }
+
+        cb(null, true);
+    },
+})
+
 
 router.get("/profile", protect, getUserProfile);
 
 router.put("/profile/update", protect, updateUserProfile)
 
 router.put("/change/password", protect, changeUserPassword)
+
+router.put("/profile/picture", protect, upload.single("profilePic"), uploadProfilePicture );
 
 export default router;
